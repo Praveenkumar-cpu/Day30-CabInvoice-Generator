a6676f1ed9c2b0b16c8a7988513a7a4ac04a1615
praveen <praveenpkbadiger@gmail.com>
2022-09-09 17:13:11 +0530

    Added: Calculate-Fare

---
+++ main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+public class CabInvoiceGenerator {
+
+    private static final double COST_PER_KM = 10;
+    private static final double COST_PER_MIN = 1;
+
+
+    public static double calculateFare(double distance,double time) {
+        double totalFare = COST_PER_KM * distance + COST_PER_MIN * time;
+        return totalFare <= 5 ? 5 : totalFare;
+    }
+
+
+    }
+
